174b2da9b6be21cc67f146b8b311cace1b4ae912
Jake <jakesenior92@googlemail.com>
2021-03-27 09:57:20 +0100

    Base Player Implementation

---
+++ rps/player.py
@@ -0,0 +1,25 @@
+"""
+This class defines the main Player class. An initial Player is defined and then a User and AI players will inherit
+from that class.
+"""
+
+import random
+
+
+class Player:
+    def __init__(self):
+        self.name = ''
+        self.current_score = 0
+        self.move_set = {'r': 1, 'p': 2, 's': 3}
+
+
+class HumanPlayer(Player):
+    def __init__(self):
+        super().__init__()
+        self.password = ''
+
+
+class MediumAIPlayer(Player):
+    def random_move(self):
+        move = random.choice(list(self.move_set.values()))
+        return move
