ab56948639fedc4c47bf585d685c523e21412569
Jake <jakesenior92@googlemail.com>
2021-03-28 19:13:35 +0200

    Added some tests for the game module

--- rps/tests/test_game.py
+++ rps/tests/test_game.py
@@ -5,18 +5,17 @@ import player
 
 class TestGame(unittest.TestCase):
 
-    def setUp(self):
-        human = player.HumanPlayer('Jake')
-        game = g.Game(1, human)
+    human = player.HumanPlayer('Jake')
+    game = g.Game(1, human)
 
-    def test_human_player_wins(self):
+    def test_human_player_wins(self, game):
         result = game.round_result('r', 'p')
         self.assertEqual(result, 1)
 
-    def test_ai_player_wins(self):
+    def test_ai_player_wins(self, game):
         result = game.round_result('p', 's')
         self.assertEqual(result, -1)
 
-    def test_both_moves_the_same(self):
+    def test_both_moves_the_same(self, game):
         result = game.round_result('r', 'p')
         self.assertEqual(result, 0)
