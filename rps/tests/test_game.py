7e487c383d193df344305ecd2beea0d6878668e4
Jake <jakesenior92@googlemail.com>
2021-03-29 08:39:31 +0200

    Unittests now work

--- rps/tests/test_game.py
+++ rps/tests/test_game.py
@@ -4,18 +4,20 @@ import player
 
 
 class TestGame(unittest.TestCase):
-
     human = player.HumanPlayer('Jake')
     game = g.Game(1, human)
+    def setUp(self):
+        human = player.HumanPlayer('Jake')
+        game = g.Game(1, human)
 
-    def test_human_player_wins(self, game):
-        result = game.round_result('r', 'p')
+    def test_human_player_wins(self):
+        result = self.game.round_result('r', 's')
         self.assertEqual(result, 1)
 
-    def test_ai_player_wins(self, game):
-        result = game.round_result('p', 's')
+    def test_ai_player_wins(self):
+        result = self.game.round_result('p', 's')
         self.assertEqual(result, -1)
 
-    def test_both_moves_the_same(self, game):
-        result = game.round_result('r', 'p')
+    def test_both_moves_the_same(self):
+        result = self.game.round_result('r', 'r')
         self.assertEqual(result, 0)
